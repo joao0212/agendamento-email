b8c2bfc3decfa3e8eb685a2ff705f4faac30960b
João Victor <jvc.martins12@gmail.com>
2020-07-15 17:27:47 -0300

    Ajustando aplicação por conta da inversão das aulas

--- src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -1,13 +1,8 @@
 package br.com.alura.servico;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
 
 @Stateless
 public class AgendamentoEmailServico {
 
-	public List<String> listar() {
-		return List.of("joao@alura.com.br", "ana@alura.com.br");
-	}
 }
