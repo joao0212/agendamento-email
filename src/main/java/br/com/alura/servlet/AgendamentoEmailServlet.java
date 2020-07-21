7e2e346583f240c9f38e48e7ac9ec10719b4203d
João Victor <jvc.martins12@gmail.com>
2020-07-21 15:43:11 -0300

    Alteração na servlet fazendo chamada para o EJB

--- src/main/java/br/com/alura/servlet/AgendamentoEmailServlet.java
+++ src/main/java/br/com/alura/servlet/AgendamentoEmailServlet.java
@@ -3,22 +3,26 @@ package br.com.alura.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("ola-mundo")
-public class OlaMundoServlet extends HttpServlet {
+import br.com.alura.servico.AgendamentoEmailServico;
+
+@WebServlet("emails")
+public class AgendamentoEmailServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private AgendamentoEmailServico servico;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter pw = resp.getWriter();
-		pw.print("Olá, mundo!");
-		pw.close();
+		servico.listar().forEach(resultado -> pw.print("Os e-mails disponíveis são: " + resultado));
 	}
-
 }
