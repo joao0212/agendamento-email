2ad991652fcc10e4f46ced06289fc4faf2770962
João Victor <jvc.martins12@gmail.com>
2020-08-05 18:05:04 -0300

    Habilitando transação com EJB

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.inject.Inject;
 import javax.jms.JMSConnectionFactory;
 import javax.jms.JMSContext;
@@ -14,22 +18,23 @@ import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
 
 @Singleton
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class AgendamentoEmailJob {
-	
+
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
-	
+
 	@Inject
 	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
 	private JMSContext context;
-	
+
 	@Resource(mappedName = "java:/jms/queue/EmailQueue")
 	private Queue queue;
-	
+
 	@Schedule(hour = "*", minute = "*", second = "*/10")
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void enviarEmail() {
-		List<AgendamentoEmail> listarPorNaoAgendado 
-			= agendamentoEmailServico.listarPorNaoAgendado();
+		List<AgendamentoEmail> listarPorNaoAgendado = agendamentoEmailServico.listarPorNaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
 			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoEmailServico.alterar(emailNaoAgendado);
