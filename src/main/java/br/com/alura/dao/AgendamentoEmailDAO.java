2301562feea327ed924d9282843386a38340b790
João Victor <jvc.martins12@gmail.com>
2020-08-14 11:14:31 -0300

    Controle transacional manual

--- src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
+++ src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
@@ -3,16 +3,24 @@ package br.com.alura.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.alura.entidade.AgendamentoEmail;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class AgendamentoEmailDAO {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Inject
+	private UserTransaction userTransaction;
 
 	public List<AgendamentoEmail> listar() {
 		return entityManager.createQuery("SELECT ae FROM AgendamentoEmail ae", AgendamentoEmail.class).getResultList();
@@ -29,6 +37,13 @@ public class AgendamentoEmailDAO {
 	}
 
 	public void alterar(AgendamentoEmail agendamentoEmail) {
-		entityManager.merge(agendamentoEmail);
+		try {
+			userTransaction.begin();
+			entityManager.merge(agendamentoEmail);
+			userTransaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
